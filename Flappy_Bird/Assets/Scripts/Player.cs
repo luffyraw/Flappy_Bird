61594e0afa9bd7b9f8ca9025db53aae486656775
luffyraw <92080765+luffyraw@users.noreply.github.com>
2021-12-11 15:58:41 +0700

    add game

---
+++ Flappy_Bird/Assets/Scripts/Player.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D Bird;
+    private SpriteRenderer spriteRenderer;
+    public Sprite[] sprites;
+    private int spriteIndex;
+
+    public float VelocityPerJump = 3f;
+    public float RotateUpSpeed = 1, RotateDownSpeed = 1;
+
+
+    FlappyYAxisTravelState flappyYAxisTravelState;
+
+    enum FlappyYAxisTravelState
+    {
+        GoingUp, GoingDown
+    }
+    
+    Vector3 birdRotation = Vector3.zero;
+
+    private void Awake()
+    {
+        Bird = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    private void OnEnable()
+    {
+        Vector3 position = Bird.transform.position;
+        position.y = 0f;
+        Bird.transform.position = position;
+    }
+    private void Start()
+    {
+        InvokeRepeating(nameof(AnimateSprite), 0.15f, 0.15f);
+    }
+    private void Update()
+    {
+       
+        if (Input.GetMouseButtonDown(0))
+        {
+            Bird.velocity = new Vector2(0, VelocityPerJump);
+        }
+        
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                Bird.velocity = new Vector2(0, VelocityPerJump);
+            }
+        }
+
+    }
+    private void FixedUpdate()
+    {
+        FixFlappyRotation();
+    }
+    private void AnimateSprite()
+    {
+        spriteIndex++;
+        if(spriteIndex >= sprites.Length)
+        {
+            spriteIndex = 0;
+        }
+        spriteRenderer.sprite = sprites[spriteIndex];
+    }
+
+    private void FixFlappyRotation()
+    {
+        if (Bird.velocity.y > 0) 
+            flappyYAxisTravelState = FlappyYAxisTravelState.GoingUp;
+        else 
+            flappyYAxisTravelState = FlappyYAxisTravelState.GoingDown;
+
+        float degreesToAdd = 0;
+
+        switch (flappyYAxisTravelState)
+        {
+            case FlappyYAxisTravelState.GoingUp:
+                degreesToAdd = 6 * RotateUpSpeed;
+                break;
+            case FlappyYAxisTravelState.GoingDown:
+                degreesToAdd = -3 * RotateDownSpeed;
+                break;
+            default:
+                break;
+        }
+        //solution with negative eulerAngles found here: http://answers.unity3d.com/questions/445191/negative-eular-angles.html
+
+        //clamp the values so that -90<rotation<45 *always*
+        birdRotation = new Vector3(0, 0, Mathf.Clamp(birdRotation.z + degreesToAdd, -90, 45));
+        transform.eulerAngles = birdRotation;
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Obstacle")
+        {
+            FindObjectOfType<GameManager>().GameOver();
+        } else if(other.gameObject.tag == "Scoring")
+        {
+            FindObjectOfType<GameManager>().IncreaseScore();
+        }
+    }
+}
